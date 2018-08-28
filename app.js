190398ab7765de1ca773773f4f0b2e676a2055b6
徐瑞 <xurui@sunline.cn>
2018-08-29 00:22:16 +0800

    提交

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+
+import allreq from './request/allrequest'
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -9,7 +11,9 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        this.getOpenID(res.code)
       }
     })
     // 获取用户信息
@@ -19,6 +23,7 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              // console.log(res);
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
@@ -35,5 +40,57 @@ App({
   },
   globalData: {
     userInfo: null
-  }
+  },
+
+  //获取openid
+  getOpenID(code) {
+    let that = this;
+    wx.request({
+      url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx4d9cf8e37e8715d0&secret=a0386ca0fc6e71389f7440e281afaf44&js_code='+ code +'&grant_type=authorization_code',
+      data: {},
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res)
+        // that.setOpenID(res.data.openid)
+        that.allreq.setOpenID(res.data.openid).then(res => {
+          console.log(res)
+        })
+        // that.getToken()
+        // openid = res.data.openid //返回openid
+      }
+    })
+  },
+  
+  // 获取access_token
+  getToken() {
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx4d9cf8e37e8715d0&secret=a0386ca0fc6e71389f7440e281afaf44',
+      data: {},
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res)
+        // openid = res.data.openid //返回openid
+      }
+    })
+  },
+
+  // 传openid
+  setOpenID (openID) {
+    wx.request({
+      url: 'http://api.jiaozi.twad.club:8081/api/services/app/WX/SaveUserWXInfo?openID=' + openID,
+      data: {},
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res)
+        // openid = res.data.openid //返回openid
+      }
+    })
+  },
+  allreq:new allreq()
 })
--- pages/chshop/chshop.js
+++ pages/chshop/chshop.js
@@ -5,6 +5,7 @@ Page({
      * 页面的初始数据
      */
     data: {
+      imgUrl: 'http://image.twad.club/',
       shopList:[
         {
           title: "袁记饺子知音路店",
